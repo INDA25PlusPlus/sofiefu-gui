3b05fb711992e26f4d62a5ccb288850188801c21
sofie114 <slushtre@gmail.com>
2025-09-30 21:44:27 +0200

    networking

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
+// chess library
 use lachess::*;
 use lachess::{Board, Position, MoveResult, Piece, PieceType, PieceColor};
 
+// gui
 use ggez::graphics::{self, Color, Rect, DrawMode, Mesh, Image, DrawParam};
-use ggez::{Context, ContextBuilder, GameResult};
+use ggez::{Context, ContextBuilder, GameResult, GameError};
 use ggez::event::{self, EventHandler, MouseButton};
 use ggez::input::keyboard::KeyInput;
 use std::collections::HashMap;
-
+use std::char::from_digit;
 const side: f32 = 60.0; // pixels per square
+
+// networking
+use std::net::TcpStream;
+mod network;
 
 struct MainState {
     board: lachess::Board, 
     images: HashMap<String, Image>,
     start: Option<lachess::Position>, 
     end: Option<lachess::Position>,
+    stream: TcpStream,
+    my_color: bool, // black=0, white=1
+    my_turn: bool,
 }
 
+// gui stuff
 impl MainState {
-    pub fn new(ctx: &mut ggez::Context) -> MainState {
+    pub fn new(ctx: &mut ggez::Context, my_color: bool, my_turn: bool, stream: TcpStream) -> MainState {
         MainState {
             board: lachess::Board::starting_position(),
             images: HashMap::from([
@@ -38,6 +48,9 @@ impl MainState {
             ]),
             start: None,
             end: None,
+            stream,
+            my_color,
+            my_turn,
         }
     }
 
@@ -60,9 +73,146 @@ impl MainState {
     }
 }   
 
+impl MainState {
+    pub fn get_character(&mut self, piece: lachess::Piece) -> char {
+        match piece {
+            lachess::Piece{ type_: lachess::PieceType::Pawn, color: lachess::PieceColor::Black }   => 'p',
+            lachess::Piece{ type_: lachess::PieceType::Knight, color: lachess::PieceColor::Black }   => 'n',
+            lachess::Piece{ type_: lachess::PieceType::Bishop, color: lachess::PieceColor::Black }   => 'b',
+            lachess::Piece{ type_: lachess::PieceType::Rook, color: lachess::PieceColor::Black }   => 'r',
+            lachess::Piece{ type_: lachess::PieceType::Queen, color: lachess::PieceColor::Black }   => 'q',
+            lachess::Piece{ type_: lachess::PieceType::King, color: lachess::PieceColor::Black }   => 'k',
+
+            lachess::Piece{ type_: lachess::PieceType::Pawn, color: lachess::PieceColor::White }   => 'P',
+            lachess::Piece{ type_: lachess::PieceType::Knight, color: lachess::PieceColor::White }   => 'N',
+            lachess::Piece{ type_: lachess::PieceType::Bishop, color: lachess::PieceColor::White }   => 'B',
+            lachess::Piece{ type_: lachess::PieceType::Rook, color: lachess::PieceColor::White }   => 'R',
+            lachess::Piece{ type_: lachess::PieceType::Queen, color: lachess::PieceColor::White }   => 'Q',
+            lachess::Piece{ type_: lachess::PieceType::King, color: lachess::PieceColor::White }   => 'K',
+        }
+    }
+
+    pub fn generate_message(&mut self, promotion: bool, start_r: i8, start_c: i8, end_r: i8, end_c: i8) -> String {
+        let mut msg = String::from("ChessMOVE:");
+
+        // send move - 1-indexed + promotion
+        msg.push((start_c as u8+65) as char); // 65 --> 'A'
+        if let Some(sr) = from_digit((start_r+1) as u32, 10) { msg.push(sr); }
+        msg.push((end_c as u8+65) as char); 
+        if let Some(er) = from_digit((end_r+1) as u32, 10) { msg.push(er); }
+
+        if promotion { msg.push('q'); }
+        else { msg.push('0'); }
+        msg.push(':');
+
+
+        // game state
+        if self.board.is_checkmate() { 
+            if (self.my_color && self.my_turn) || (!self.my_color && !self.my_turn) { msg.push_str("1-0"); }
+            else { msg.push_str("0-1"); }
+        }
+        else if self.board.is_stalemate() {msg.push_str("1-1"); }
+        else { msg.push_str("0-0"); }
+        msg.push(':');
+
+
+        // send board
+        for row in (0..8).rev() {
+            let mut ct: u32 = 0;
+            for col in 0..8 {
+                let pos = lachess::Position{
+                    file: col as i8,
+                    rank: row as i8,
+                };
+
+                if let Some(piece) = self.board.piece_at_pos(pos) {
+                    if ct>0 && let Some(digit) = from_digit(ct, 10) { msg.push(digit); }
+                    msg.push(self.get_character(piece));
+                    ct=0;
+                }
+                else if col == 7 {
+                    if ct>0 && let Some(digit) = from_digit(ct, 10) { msg.push(digit); }
+                }
+                else {
+                    ct=ct+1;
+                }
+            }
+            msg.push('/');
+        }
+        msg.push(':');
+
+        // padding
+        while msg.len() < 128 {
+            msg.push('0');
+        }
+        return msg;
+    }
+
+    pub fn rage_quit(&mut self) -> GameResult<()> {
+        let msg = "ChessQUIT::000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000".to_string();
+        network::send_message(&mut self.stream, msg);
+        return Err(GameError::EventLoopError("opponent gave invalid move".to_string())); 
+    }  
+
+    pub fn receive_move(&mut self, msg: &String) -> GameResult<()> {
+        println!("mottagit drag");
+        if &msg[0..9] == "ChessQUIT" { return Err(GameError::EventLoopError("opponent quited".to_string())); } 
+
+        // decode move
+        let mut from = lachess::Position{ file: 10, rank: 10 };
+        let mut to = lachess::Position{ file: 10, rank: 10 };
+        if let Some(start_c) = msg.chars().nth(9) && let Some(start_r) = msg.chars().nth(10) {
+            from.rank = ((start_c as u32)-65) as i8;
+            from.file = ((start_r as u32)-48) as i8;
+        } 
+        if let Some(end_c) = msg.chars().nth(11)  && let Some(end_r) = msg.chars().nth(12) {
+            to.rank = ((end_c as u32)-65) as i8;
+            to.file = ((end_r as u32)-48) as i8;
+        } 
+
+        match self.board.make_move(from, to) {
+            MoveResult::Normal => {
+                let my_msg = self.generate_message(false, from.rank, from.file, to.rank, to.file);
+                
+                let mut ct = 0; let mut i = 0;
+                while i < 128 {
+                    if let Some(chA) = my_msg.chars().nth(i) && let Some(chB) = msg.chars().nth(i) { if chA != chB { self.rage_quit(); } } 
+                    if let Some(ch) = my_msg.chars().nth(i){ if ch == ':' {ct+=1;} }
+                    if ct == 4 {break;}
+                }
+            },
+            MoveResult::Promotion => {
+                // change to the one they send
+                self.board.resolve_promotion(PieceType::Queen).unwrap();
+                let my_msg = self.generate_message(true, from.rank, from.file, to.rank, to.file);
+
+                let mut ct = 0; let mut i = 0;
+                while i < 128 {
+                    if let Some(chA) = my_msg.chars().nth(i) && let Some(chB) = msg.chars().nth(i) { if chA != chB { self.rage_quit(); } } 
+                    if let Some(ch) = my_msg.chars().nth(i){ if ch == ':' {ct+=1;} }
+                    if ct == 4 {break;}
+                }
+            },
+            MoveResult::Illegal => {
+                return self.rage_quit();
+            }
+        }
+        return Ok(());
+    }
+
+}
+
+
 impl ggez::event::EventHandler for MainState {
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
-        Ok(())
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        // vänta på meddelande från motståndare
+        if let Some(msg) = network::try_receive_message(&mut self.stream) {
+            if self.my_turn { self.rage_quit(); }
+            let res = self.receive_move(&msg);
+            self.my_turn = true;
+            return res;
+        }
+        return Ok(());
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::from_rgb(200, 200, 200));
@@ -83,20 +233,27 @@ impl ggez::event::EventHandler for MainState {
 
 
                 // selected squares
-                if let Some(from) = self.start && let Some(to) = self.end { // move made
+                if let Some(from) = self.start && let Some(to) = self.end && self.my_turn { // move made
                     println!("making move");
                     match self.board.make_move(from, to) {
                         MoveResult::Normal => {
-                            println!("Move made");
-                            self.start = None;
-                            self.end = None;
+                            println!("move made");
+                            self.start = None; self.end = None; 
+                            let msg = self.generate_message(false, from.rank, from.file, to.rank, to.file);
+                            network::send_message(&mut self.stream, msg);
+                            self.my_turn = false;
                         },
                         MoveResult::Promotion => {
-                            println!("Pawn promotion!");
+                            println!("pawn promotion!");
                             self.board.resolve_promotion(PieceType::Queen).unwrap();
+                            let msg = self.generate_message(true, from.rank, from.file, to.rank, to.file);
+
+                            network::send_message(&mut self.stream, msg);
+                            self.my_turn = false;
+
                         },
                         MoveResult::Illegal => {
-                            println!("Illegal move");
+                            println!("illegal move");
                             self.start = None;
                             self.end = None;
                         }
@@ -126,10 +283,6 @@ impl ggez::event::EventHandler for MainState {
                 canvas.draw(&rect, [0.0, 0.0]);
 
                 // DRAW PIECES
-                let pos = lachess::Position{
-                    file: col as i8,
-                    rank: row as i8,
-                };
                 if let Some(piece) = self.board.piece_at_pos(pos) {
                     let piece_type = piece.type_;
     
@@ -188,15 +341,32 @@ impl ggez::event::EventHandler for MainState {
 }
 
 pub fn main() {
-    println!("Hello, world!");
-    
     let (mut ctx, event_loop) = ContextBuilder::new("hello_ggez", "Sofie")
     .add_resource_path("./resources")
     .build()
     .unwrap();
 
-    let state = MainState::new(&mut ctx); 
-    event::run(ctx, event_loop, state);
+    // PLAY AS WHITE (CLIENT)
+    // match network::start_client() {
+    //     Ok(stream) => {
+    //         let state = MainState::new(&mut ctx, true, ture, stream); 
+    //         event::run(ctx, event_loop, state);
+    //     }
+    //     Err(e) => {
+    //         println!("failed to start server");
+    //     }
+    // }
+
+    // PLAY AS BLACK (SERVER)
+    match network::start_server() {
+        Ok(stream) => {
+            let state = MainState::new(&mut ctx, false, false, stream); 
+            event::run(ctx, event_loop, state);
+        }
+        Err(e) => {
+            println!("failed to start server");
+        }
+    }
 }
 
 
---
+++ src/network.rs
@@ -0,0 +1,40 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+use std::io::{Read, Write};
+
+const addr: &str = "127.0.0.1:8080";
+// serversida, startar först, Tcplistener för att vänta på anslutningar
+pub fn start_server() -> std::io::Result<TcpStream> {
+    let listener = TcpListener::bind(addr)?;
+
+    let (stream, client_addr) = listener.accept()?; // blocks until a client connects
+    println!("client connected");
+    return Ok(stream);
+}
+
+// klientsidan, startar efter servern, använder Tcpstream::connect för att ansluta
+pub fn start_client() -> std::io::Result<TcpStream> {
+    let mut stream = TcpStream::connect(addr)?;
+    stream.set_nonblocking(true)?;
+    return Ok(stream);
+}
+
+pub fn send_message(stream: &mut TcpStream, msg: String) -> std::io::Result<()> {
+    stream.write(msg.as_bytes())?;
+    return Ok(());
+}
+
+pub fn try_receive_message(stream: &mut TcpStream) -> Option<String> {
+    let mut msg_buf = [0u8; 128];
+    match stream.read(&mut msg_buf) {
+        Ok(0) => return None, // connection closed
+        Ok(n) => return Some(String::from_utf8_lossy(&msg_buf[..n]).to_string()),
+        Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => return None,
+        Err(e) => { panic!("IO error: {e}"); }
+    };
+}
+
+
+/*
+Tips: För att testa era program lokalt kan ni använda er av loopbackadressen 127.0.0.1. 
+*/
